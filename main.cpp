def1a7d99657174e22ee12caf1465efd86b94f22
Diana Shakirova <diana.shak1rova@yandex.ru>
2025-09-15 23:25:40 +0300

    Обновила main.cpp для ввода описания

--- main.cpp
+++ main.cpp
@@ -4,27 +4,29 @@
 using namespace std;
 
 int main() {
-    //Тесты
-    // testInvalidCandidateCount();
-    // testCandidateNameWithSpaces();
-    // Ввод количества кандидатов
     int numCandidates;
     cout << "Enter the amount of candidates: ";
     cin >> numCandidates;
-    // while(!testInvalidCandidateCount(numCandidates)){
-    //     cin >> numCandidates;
-    // }
-    // Ввод имён кандидатов
+    cin.ignore();  // РћС‡РёС‰Р°РµРј Р±СѓС„РµСЂ
+
     vector<Candidate> candidates;
     for (int i = 0; i < numCandidates; i++) {
         string name;
         cout << "Enter the name of candidate #" << i + 1 << ": ";
-        cin >> name;
-        candidates.push_back(Candidate(name));
+        getline(cin, name);
+        
+        Description desc;
+        cout << "Enter interests for " << name << ": ";
+        getline(cin, desc.interests);
+        cout << "Enter achievements for " << name << ": ";
+        getline(cin, desc.achievements);
+        cout << "Enter plans for " << name << ": ";
+        getline(cin, desc.plans);
+        cout << endl;
+
+        candidates.push_back(Candidate(name, desc));
     }
 
-    // Проведение голосования
     conductElection(candidates);
-
     return 0;
 }
